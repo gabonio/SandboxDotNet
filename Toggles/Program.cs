64777462c385fee63eaf2c2f1a682a5fabd8cf0a
Gabriel Cruz <gabonio@gmail.com>
2018-10-04 09:30:30 +0200

    Added color printing test

--- Toggles/Program.cs
+++ Toggles/Program.cs
@@ -8,10 +8,12 @@ namespace Toggles
         static void Main(string[] args)
         {
             var p = new Printing();
+            var cp = new ColorPrinting();
             var s = new Saving();
             var t = new Tweeting();
 
             Console.WriteLine($"Printing is {(p.FeatureEnabled ? "on" : "off")}");
+            Console.WriteLine($"Color Printing is {(cp.FeatureEnabled ? "on" : "off")}");
             Console.WriteLine($"Saving is {(s.FeatureEnabled ? "on" : "off")}");
             Console.WriteLine($"Tweeting is {(t.FeatureEnabled ? "on" : "off")}");
 
